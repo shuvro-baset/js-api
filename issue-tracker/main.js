2423b76342badf19662fb7f1d2bdaa6d81a1594d
shuvro baset <md.abdul.baset75@gmail.com>
2021-09-09 23:34:35 +0600

    debugging done

--- issue-tracker/main.js
+++ issue-tracker/main.js
@@ -21,7 +21,7 @@ function submitIssue(e) {
   e.preventDefault();
 }
 
-const setStatusClosed = id => {
+const closeIssue = id => {
   const issues = JSON.parse(localStorage.getItem('issues'));
   const currentIssue = issues.find(issue => +issue.id === id);
   currentIssue.status = 'Closed';
@@ -31,16 +31,19 @@ const setStatusClosed = id => {
 
 const deleteIssue = id => {
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const remainingIssues = issues.filter(issue=> +issue.id !== id )
-  console.log(remainingIssues)
+  const remainingIssues = issues.filter(issue => +issue.id !== id)
   localStorage.setItem('issues', JSON.stringify(remainingIssues));
-  // fetchIssues()
+  location.reload();
 }
 
 const fetchIssues = () => {
   const issues = JSON.parse(localStorage.getItem('issues'));
+  if(issues===null){
+    console.log('no issues found')
+    return;
+  }
   const issuesList = document.getElementById('issuesList');
-  // issuesList.innerHTML = '';
+  issuesList.innerHTML = '';
 
   for (var i = 0; i < issues.length; i++) {
     const {id, description, severity, assignedTo, status} = issues[i];
@@ -51,7 +54,7 @@ const fetchIssues = () => {
                               <h3> ${description} </h3>
                               <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
                               <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
-                              <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
+                              <a href="#" onclick="closeIssue(${id})" class="btn btn-warning">Close</a>
                               <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
                               </div>`;
   }
