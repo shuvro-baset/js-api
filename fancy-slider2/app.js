51f8a3e93f20f11a0d9b58823c3aed58c9ebfdc6
shuvro baset <md.abdul.baset75@gmail.com>
2021-09-12 09:01:48 +0600

    fancy slider debugging done

--- fancy-slider2/app.js
+++ fancy-slider2/app.js
@@ -29,6 +29,7 @@ const showImages = (images) => {
 }
 
 const getImages = (query) => {
+  console.log(query)
   fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
     .then(response => response.json())
     .then(data => showImages(data.hits))
